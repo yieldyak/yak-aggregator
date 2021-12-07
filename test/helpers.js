6017dc756d682929c34e886c0edc153652d72666
Miha <mihalo3c@gmail.com>
2021-12-07 22:57:59 +0100

    detect token slot

--- test/helpers.js
+++ test/helpers.js
@@ -1,7 +1,7 @@
 const { ethers } = require("hardhat")
+const fs = require('fs')
 
 const bigNumToBytes32 = (bn) => {
-    console.log(bn)
     return ethers.utils.hexlify(ethers.utils.zeroPad(bn.toHexString(), 32))
 };
   
@@ -12,6 +12,45 @@ const addressToBytes32 = (add) => {
 const setStorageAt = async (address, index, value) => {
     await ethers.provider.send("hardhat_setStorageAt", [address, index, value]);
 };
+
+const getERC20SlotByExecute = async (token, _signer) => {
+    const signer = _signer || (await ethers.getSigners())[0]
+    const holder = signer.address
+    const tx = await signer.sendTransaction({
+        data: `0x70a08231000000000000000000000000${holder.slice(2)}`, 
+        gasPrice: ethers.utils.parseUnits('225', 'gwei'),
+        to: token, 
+    })
+    const result = await signer.provider.send('debug_traceTransaction', [tx.hash])
+    const [ slot ] = result.structLogs.flatMap(log => {
+        // Find SLOAD operations containing the holder address
+        const addressMatches = () => '0x'+log.memory[0].replace(/^0+/, '') == signer.address.toLowerCase()
+        if (log.op == 'SLOAD' && addressMatches()) {
+            const hash = ethers.utils.keccak256('0x' + log.memory[0] + log.memory[1])
+            // Return the slot if its hash with holder address is mapped to return value in storage and hash is on top of stack
+            if (log.stack[-1] == hash.slice(2) == result.returnValue) {
+                return parseInt(log.memory[1])
+            }
+        }
+        return []
+    })
+    return slot ?? null
+}
+
+const getERC20Slot = async (token, signer) => {
+    const cacheTarget = `${process.cwd()}/cache/slots/${token}`
+    try {
+        const savedSlot = fs.readFileSync(cacheTarget)
+        return parseInt(savedSlot.toString())
+    } catch (_) {
+        const executedSlot = await getERC20SlotByExecute(token, signer)
+        if (executedSlot != null) {
+            fs.writeFile(cacheTarget, executedSlot.toString(), () => {})
+            return executedSlot
+        }
+        throw new Error(`Could not find ERC20 slot for token ${token}`)
+    }
+}
 
 module.exports.makeAccountGen = async () => {
     function* getNewAccount() {
@@ -68,8 +107,9 @@ module.exports.getTokenContract = tokenAddress => ethers.getContractAt(
     tokenAddress
 )
 
-module.exports.setERC20Bal = async (token, _amount, _holder, _storageSlot) => {
+module.exports.setERC20Bal = async (_token, _holder, _amount) => {
+    const storageSlot = await getERC20Slot(_token)
     const key = addressToBytes32(ethers.BigNumber.from(_holder.toString()))
-    const index = ethers.utils.solidityKeccak256([ "uint256", "uint256" ], [ key, _storageSlot ])
-    await setStorageAt(token, index, bigNumToBytes32(_amount))
+    const index = ethers.utils.solidityKeccak256([ "uint256", "uint256" ], [ key, storageSlot ])
+    await setStorageAt(_token, index, bigNumToBytes32(_amount))
 }
