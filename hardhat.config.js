f0f6f00f45b595546ff0987a98cbd37da311e53a
Miha <mihalo3c@gmail.com>
2022-03-03 11:07:18 +0100

    add task to list live adapters

--- hardhat.config.js
+++ hardhat.config.js
@@ -32,6 +32,19 @@ task("verifyContract", "Verifies the contract in the snowtrace")
     async({deploymentFilePath}, hre) => verifyContract(deploymentFilePath, hre)
   )
 
+task("list-adapters", "Lists all adapters for the current YakRouter", async (_, hre) => {
+  const YakRouter = await hre.ethers.getContract("YakRouterV0")
+  const adapterLen = await YakRouter.adaptersCount()
+  const adapterIndices = Array.from(Array(adapterLen.toNumber()).keys())
+  const liveAdapters = await Promise.all(adapterIndices.map(async (i) => {
+      const adapter = await YakRouter.ADAPTERS(i)
+      const name = await hre.ethers.getContractAt("YakAdapter", adapter)
+        .then(a => a.name())
+      return { adapter, name }
+  }))
+  console.table(liveAdapters)
+})
+
 module.exports = {
   mocha: {
     timeout: 1e6,
