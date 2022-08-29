507008098ef62520bfba7d5a2657d79961960111
halo3mic <46010359+halo3mic@users.noreply.github.com>
2022-08-29 18:42:57 +0100

    Update dependencies

--- contracts/test/NTInjector.sol
+++ contracts/test/NTInjector.sol
@@ -3,9 +3,7 @@ pragma solidity >=0.7.0;
 
 // Inject native-token into contract without fallback function
 contract NTInjector {
-
     constructor(address payable _reciever) payable {
         selfdestruct(_reciever);
     }
-
 }
