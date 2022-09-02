20fa605f09f18c4c6e271dda85af6cd984fca532
halo3mic <46010359+halo3mic@users.noreply.github.com>
2022-09-02 17:16:14 +0100

    fix addresses ref

--- src/deploy/avalanche/adapters/synapse/synapse.js
+++ src/deploy/avalanche/adapters/synapse/synapse.js
@@ -1,4 +1,4 @@
-const { curvelikePools } = require("../../../test/addresses.json").avalanche  
+const { curvelikePools } = require("../../../../misc/addresses.json").avalanche  
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
