b053d5e1ccb2ac8812a270d5426b9b07557ef8e6
halo3mic <46010359+halo3mic@users.noreply.github.com>
2022-09-26 19:34:47 +0100

    readd elk adapter

--- src/misc/deployOptions.js
+++ src/misc/deployOptions.js
@@ -10,6 +10,7 @@ module.exports = {
             'PangolinAdapter',
             'SushiswapAdapter',
             'KyberAdapter',
+            'ElkAdapter',
       
             'SynapseAdapter',
             'AxialAS4DAdapter',
