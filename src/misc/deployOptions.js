a9bde794adbab3acbc18fb015a1cdc27232ec021
halo3mic <46010359+halo3mic@users.noreply.github.com>
2022-11-08 13:24:13 +0000

    rm arable from router wl

--- src/misc/deployOptions.js
+++ src/misc/deployOptions.js
@@ -30,7 +30,6 @@ module.exports = {
             'KyberElasticAdapter',
             'WoofiUSDCAdapter',
             'GeodeWPAdapter',
-            'ArableAdapter',
             'GmxAdapter',
             
             'MiniYakAdapter',
