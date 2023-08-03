b5113ee8789fb7481522c55e71227b7256358937
halo3mic <46010359+halo3mic@users.noreply.github.com>
2023-08-03 20:09:30 +0100

    disable solisnek-adapter

--- src/misc/deployOptions.js
+++ src/misc/deployOptions.js
@@ -12,7 +12,7 @@ module.exports = {
             'PangolinAdapter',
             'SushiswapAdapter',
             'GlacierAdapter',
-            'SolisnekAdapter',
+            // 'SolisnekAdapter',
       
             'SynapseAdapter',
             'AxialAS4DAdapter',
