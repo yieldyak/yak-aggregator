040a9c64409faf39b9c6ed95bcdb21c55c0aa875
A.L <xenide@users.noreply.github.com>
2023-10-10 21:46:45 -0500

    test: update test case

--- src/test/spec/avalanche/adapters/reservoir.spec.js
+++ src/test/spec/avalanche/adapters/reservoir.spec.js
@@ -1,3 +1,4 @@
+const { expect } = require("chai")
 const { setTestEnv, addresses } = require('../../../utils/test-env')
 const { reservoir } = addresses.avalanche
 
@@ -29,27 +30,30 @@ describe('YakAdapter - Reservoir', () => {
     })
 
     describe('Swapping matches query', async () => {
-
         it('100 USDt -> USDC', async () => {
             await ate.checkSwapMatchesQuery('100', tkns.USDt, tkns.USDC)
         })
         it('1 USDC -> BTC.b', async () => {
             await ate.checkSwapMatchesQuery('10', tkns.USDC, tkns.BTCb)
         })
-
     })
 
     it('Query returns zero if tokens not found', async () => {
         const supportedTkn = tkns.USDC
-        ate.checkQueryReturnsZeroForUnsupportedTkns(supportedTkn)
+        await ate.checkQueryReturnsZeroForUnsupportedTkns(supportedTkn)
+    })
+
+    it ('Query returns something for a valid pair', async () => {
+        // 1 USDC
+        const amountOutQuery = await ate.query('1000000', tkns.USDC.address, tkns.USDt.address)
+        expect(amountOutQuery).gt(0)
     })
 
     it('Gas-estimate is between max-gas-used and 110% max-gas-used', async () => {
         const options = [
-            [ '100', tkns.USDT, tkns.USDC ],
-            [ '100', tkns.USDC, tkns.BTCb ],
+            [ '100', tkns.USDt, tkns.USDC ],
+            [ '1', tkns.USDC, tkns.BTCb ],
         ]
         await ate.checkGasEstimateIsSensible(options)
     })
-
 })
