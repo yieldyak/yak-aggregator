31c991b7887dbd6f2f2ae83ae7ac48fd5f8d7482
halo3mic <46010359+halo3mic@users.noreply.github.com>
2023-12-04 01:54:42 +0000

    deploy wombat

--- src/misc/deployOptions.js
+++ src/misc/deployOptions.js
@@ -35,6 +35,7 @@ module.exports = {
             
             'SAvaxAdapter',
             'WAvaxAdapter',
+            'WombatAdapter',
         ],
         minimalAdapterWhitelist: [
             'WAvaxAdapter',
