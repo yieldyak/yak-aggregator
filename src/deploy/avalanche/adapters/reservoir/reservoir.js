7ef4ebac9b407a1f51184558e4aa972fed233336
halo3mic <46010359+halo3mic@users.noreply.github.com>
2023-12-04 01:54:02 +0000

    fix reservoir deploy script

--- src/deploy/avalanche/adapters/reservoir/reservoir.js
+++ src/deploy/avalanche/adapters/reservoir/reservoir.js
@@ -1,12 +1,10 @@
 const { deployAdapter, addresses } = require('../../../utils')
-const { reservoir } = addresses.reservoir
+const { factory, quoter } = addresses.avalanche.reservoir
 
 const networkName = 'avalanche'
 const tags = [ 'reservoir', 'stableswap' ]
 const name = 'ReservoirAdapter'
 const contractName = 'ReservoirAdapter'
-const factory = reservoir.factory
-const quoter = reservoir.quoter
 
 const gasEstimate = 350_000
 const args = [ name, factory, quoter, gasEstimate ]
