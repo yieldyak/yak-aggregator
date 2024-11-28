04497f34536f161c10200027470bae0b6a055e6c
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2024-11-28 13:10:25 +0100

    update GGAvaxAdapter (#111)

--- src/contracts/adapters/GGAvaxAdapter.sol
+++ src/contracts/adapters/GGAvaxAdapter.sol
@@ -28,7 +28,8 @@ interface IWAVAX {
 
 /**
  * @notice WAVAX -> ggAVAX
- **/
+ *
+ */
 contract GGAvaxAdapter is YakAdapter {
     address public constant ggAVAX = 0xA25EaF2906FA1a3a13EdAc9B9657108Af7B703e3;
     address public constant WAVAX = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
@@ -37,17 +38,20 @@ contract GGAvaxAdapter is YakAdapter {
         IERC20(WAVAX).approve(ggAVAX, type(uint256).max);
     }
 
-    function _query(
-        uint256 _amountIn,
-        address _tokenIn,
-        address _tokenOut
-    ) internal view override returns (uint256 amountOut) {
+    function _query(uint256 _amountIn, address _tokenIn, address _tokenOut)
+        internal
+        view
+        override
+        returns (uint256 amountOut)
+    {
         if (_tokenIn == WAVAX && _tokenOut == ggAVAX) {
             if (_amountIn > IGGAvax(ggAVAX).maxDeposit(address(this))) return 0;
             return IGGAvax(ggAVAX).previewDeposit(_amountIn);
         } else if (_tokenIn == ggAVAX && _tokenOut == WAVAX) {
             amountOut = IGGAvax(ggAVAX).previewRedeem(_amountIn);
-            uint256 avail = IGGAvax(ggAVAX).totalAssets() - IGGAvax(ggAVAX).stakingTotalAssets();
+            uint256 totalAssets = IGGAvax(ggAVAX).totalAssets();
+            uint256 stakingTotalAssets = IGGAvax(ggAVAX).stakingTotalAssets();
+            uint256 avail = totalAssets > stakingTotalAssets ? totalAssets - stakingTotalAssets : 0;
             return amountOut > avail ? avail : amountOut;
         }
     }
