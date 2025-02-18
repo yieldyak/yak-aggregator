05ab430cc0ea3bcb61f65ebd1a9aa83fec120abb
A.L. <xenide@users.noreply.github.com>
2025-02-18 11:06:16 +0700

    fix: update gas estimates

--- src/deploy/arbitrum/adapters/reservoir/reservoir.js
+++ src/deploy/arbitrum/adapters/reservoir/reservoir.js
@@ -6,7 +6,7 @@ const tags = [ 'reservoir', 'stableswap' ]
 const name = 'ReservoirAdapter'
 const contractName = 'ReservoirAdapter'
 
-const gasEstimate = 350_000
+const gasEstimate = 200_000
 const args = [ name, factory, quoter, gasEstimate ]
 
 module.exports = deployAdapter(networkName, tags, name, contractName, args)
