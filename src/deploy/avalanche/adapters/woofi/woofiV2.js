3d4a7319451fd3e2b029724a380b1cd62018fe21
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2025-04-03 12:03:55 +0200

    update Woofi adapters

--- src/deploy/avalanche/adapters/woofi/woofiV2.js
+++ src/deploy/avalanche/adapters/woofi/woofiV2.js
@@ -1,5 +1,5 @@
 const { deployAdapter, addresses } = require('../../../utils')
-const { WoofiV2Pool } = addresses.avalanche.other
+const { woofiV2Pool } = addresses.avalanche.other
 
 const networkName = 'avalanche'
 const tags = [ 'woofiV2' ]
@@ -7,7 +7,7 @@ const name = 'WoofiV2Adapter'
 const contractName = 'WoofiV2Adapter'
 
 const gasEstimate = 410_000
-const pool = WoofiV2Pool
+const pool = woofiV2Pool
 const args = [ name, gasEstimate, pool, ]
 
 module.exports = deployAdapter(networkName, tags, name, contractName, args)
