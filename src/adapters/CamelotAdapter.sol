d096c50d0b40010bb510ba5a6a5cfb51864aef5f
haouvw <jchaodaohang@foxmail.com>
2025-07-21 17:16:54 +0800

    chore: fix some minor issues in comments
    Signed-off-by: haouvw <jchaodaohang@foxmail.com>

--- src/adapters/CamelotAdapter.sol
+++ src/adapters/CamelotAdapter.sol
@@ -82,7 +82,7 @@ contract CamelotAdapter is YakAdapter {
         address to
     ) internal override {
         (uint256 amountOut, address pair) = getQuoteAndPair(_amountIn, _tokenIn, _tokenOut);
-        require(amountOut >= _amountOut, "Insufficent amount out");
+        require(amountOut >= _amountOut, "Insufficient amount out");
         (uint256 amount0Out, uint256 amount1Out) = (_tokenIn < _tokenOut)
             ? (uint256(0), amountOut)
             : (amountOut, uint256(0));
